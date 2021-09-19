919595d943706f980ca48a3ac8add3d58689b64c
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2021-09-19 19:12:46 +0900

    created a moving dot
    made a folder for js files, created a moving dot

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,14 @@
-const App {
+import {
+    Wave
+ } from "./wave.js";
+
+class App {
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas)
+
+        this.wave = new Wave();
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
@@ -17,11 +23,16 @@ const App {
         this.canvas.width = this.stageWidth * 2;
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2, 2);
+
+        this.wave.resize(this.stageWidth, this.stageHeight);
     }
 
     animate(t) {
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-        requestAnimationFrame(this.anmate.bind(this));
+        
+        this.wave.draw(this.ctx);
+        
+        requestAnimationFrame(this.animate.bind(this));
     }
 }
 
