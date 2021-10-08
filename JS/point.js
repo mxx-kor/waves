bba8fe3aa425b8dd71ccc1ddb95d5fa0d7b96cda
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2021-10-08 14:55:31 +0900

    adjust the speed again

--- JS/point.js
+++ JS/point.js
@@ -3,7 +3,7 @@ export class Point {
         this.x = x;
         this.y = y;
         this.fixedY = y;
-        this.speed = 0.5;
+        this.speed = 0.08;
         this.cur = index;
         this.max = Math.random() * 100 + 150;
     }
